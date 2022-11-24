152ac3efa77d8e29495318d4d0100cc35cb98fd2
lostmart <lostmart@hotmail.com>
2022-11-24 11:38:41 +0100

    new stuff

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,33 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+	var x = document.getElementById('myTopnav')
+	if (x.className === 'topnav') {
+		x.className += ' responsive'
+	} else {
+		x.className = 'topnav'
+	}
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground')
+const modalBtn = document.querySelectorAll('.modal-btn')
+const formData = document.querySelectorAll('.formData')
+const closeBtn = document.querySelector('.close')
+const modalContent = document.querySelector('.content')
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal))
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+	modalbg.style.display = 'block'
 }
 
+let modalPosition = 0
+let modalOpacity = 1
+// close modal
+function closeModal() {
+	modalbg.style.display = 'none'
+}
 
+// listen to the click on the close btn
+closeBtn.addEventListener('click', closeModal)
